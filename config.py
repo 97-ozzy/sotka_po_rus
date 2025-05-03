f1af31b8944a93c1fd0925d5f75fb06a2694d8ee
ozzy-97 <askardtv21@gmail.com>
2025-05-03 16:35:03 +0500

    v5.4 upd error_handler.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 
 
-load_dotenv('D:/Developing/sotka_po_rus/utils/.env')
+load_dotenv('/home/askar/sotka_po_rus/utils/.env')
 
 TOKEN = os.getenv("BOT_TOKEN")
 
@@ -13,11 +13,8 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 
 
-PROVIDER_TOKEN = os.getenv('UKASSA_TOKEN')
-
 ADMIN_IDS = os.getenv("ADMIN_IDS")
+
 ADMIN_IDS = ADMIN_IDS.split(',')
 ADMIN_IDS = [ int(admin_id) for admin_id in ADMIN_IDS]
 TASKS = [4, 9, 10, 11, 12, 15]
-
-
