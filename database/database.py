22fc9604d5e3fd5fbc360c8a4057a5b4bf95b63d
ozzy-97 <askardtv21@gmail.com>
2025-05-07 03:49:37 +0500

    upd first 3 days for free

--- database/database.py
+++ database/database.py
@@ -128,6 +128,7 @@ async def get_premium_users():
 
     return premium_users_cache
 
+
 async def add_user_to_db(user_id: int, username: str):
     pool = await get_pool()
     async with pool.acquire() as conn:
@@ -135,9 +136,10 @@ async def add_user_to_db(user_id: int, username: str):
             "SELECT 1 FROM users WHERE user_id = $1", user_id
         )
         if not user_exists:
+            premium_expires_date = date.today() + timedelta(days=3)
             await conn.execute(
-                "INSERT INTO users (user_id, username) VALUES ($1, $2)",
-                user_id, username
+                "INSERT INTO users (user_id, username, premium, premium_expires_date) VALUES ($1, $2, $3, $4)",
+                user_id, username, True, premium_expires_date
             )
 
 async def submit_new_word(user_id, task_number, correct_word, wrong_word):
